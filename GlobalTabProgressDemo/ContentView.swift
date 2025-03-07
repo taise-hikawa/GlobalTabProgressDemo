f199770991a693f1919a410a3f21e5893aff7d4c
taise-hikawa <cmmkarubi@gmail.com>
2025-03-07 13:39:41 +0900

    add xcoproj

---
+++ GlobalTabProgressDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlobalTabProgressDemo
+//
+//  Created by 樋川大聖 on 2025/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlobalTabProgressDemo/GlobalTabProgressDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalTabProgressDemoApp.swift
+//  GlobalTabProgressDemo
+//
+//  Created by 樋川大聖 on 2025/03/07.
+//
+
+import SwiftUI
+
+@main
+struct GlobalTabProgressDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
